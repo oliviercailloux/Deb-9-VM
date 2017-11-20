dd08a48c5e81339cfca36dc2489ca9da0724514c
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2017-11-20 13:48:49 +0000

    sed instead of localectl

--- late_command.bash
+++ late_command.bash
@@ -12,4 +12,5 @@ vbox/VBoxLinuxAdditions.run --nox11 1>> late_command-out.txt 2>> late_command-er
 sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub 1>> late_command-out.txt 2>> late_command-errors.txt
 update-grub 1>> late_command-out.txt 2>> late_command-errors.txt
 
+sed -i 's/XKBVARIANT="latin9"/XKBVARIANT="oss_sundeadkeys"/' /etc/default/keyboard 1>> late_command-out.txt 2>> late_command-errors.txt 
 
