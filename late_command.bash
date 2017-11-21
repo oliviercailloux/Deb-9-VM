e3f5d3042d0d0d49c20002397f135debf7fd1143
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2017-11-21 10:25:11 +0000

    Simpl redir

--- late_command.bash
+++ late_command.bash
@@ -1,14 +1,28 @@
+#!/usr/bin/env bash
+
+exec > >(tee -ia /root/late_command-out.txt)
+exec 2> >(tee -ia /root/late_command-errors.txt >&2)
+
 cd /root
 
-sed -i "s/^#  \(AutomaticLoginEnable = true\)$/\1/" /etc/gdm3/daemon.conf 1>> late_command-out.txt 2>> late_command-errors.txt
-sed -i "s/^#  \(AutomaticLogin = user1\)$/AutomaticLogin = user/" /etc/gdm3/daemon.conf 1>> late_command-out.txt 2>> late_command-errors.txt
+ls /home /media /mnt
 
-mkdir vbox 1>> late_command-out.txt 2>> late_command-errors.txt
-mount /dev/sr1 vbox 1>> late_command-out.txt 2>> late_command-errors.txt
-vbox/VBoxLinuxAdditions.run --nox11 1>> late_command-out.txt 2>> late_command-errors.txt
+sed -i "s/^#  \(AutomaticLoginEnable = true\)$/\1/" /etc/gdm3/daemon.conf
+sed -i "s/^#  \(AutomaticLogin = user1\)$/AutomaticLogin = user/" /etc/gdm3/daemon.conf
 
-sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub 1>> late_command-out.txt 2>> late_command-errors.txt
-update-grub 1>> late_command-out.txt 2>> late_command-errors.txt
+mkdir vbox
+mount /dev/sr1 vbox
+vbox/VBoxLinuxAdditions.run --nox11
+
+sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
+update-grub
 
 sed -i 's/XKBVARIANT="latin9"/XKBVARIANT="oss_sundeadkeys"/' /etc/default/keyboard 1>> late_command-out.txt 2>> late_command-errors.txt 
+sed -i 's/XKBLAYOUT="fr"/XKBLAYOUT="be"/' /etc/default/keyboard 1>> late_command-out.txt 2>> late_command-errors.txt 
+
+wget http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/oxygen/1a/eclipse-jee-oxygen-1a-linux-gtk-x86_64.tar.gz 1>> late_command-out.txt 2>> late_command-errors.txt 
+tar -x -f eclipse-jee-oxygen-1a-linux-gtk-x86_64.tar.gz -C /usr/local/share/
+eclipse -nosplash -application org.eclipse.equinox.p2.director -repository "http://download.eclipse.org/releases/oxygen/,http://download.oracle.com/otn_software/oepe/12.2.1.6/oxygen/repository" -installIU oracle.eclipse.tools.glassfish.feature.group 1>> late_command-out.txt 2>> late_command-errors.txt 
+xdg-icon-resource install --size 256 /usr/local/share/eclipse/icon.xpm eclipse-oxygen
+xdg-desktop-menu install "/mnt/cdrom/eclipse.desktop"
 
