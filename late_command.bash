464df61d25ab8f97761781396fa4bc7b1a2b9528
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2017-11-21 11:51:21 +0000

    eclipse path

--- late_command.bash
+++ late_command.bash
@@ -17,13 +17,13 @@ vbox/VBoxLinuxAdditions.run --nox11
 sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
 update-grub
 
-sed -i 's/XKBVARIANT="latin9"/XKBVARIANT="oss_sundeadkeys"/' /etc/default/keyboard 1>> late_command-out.txt 2>> late_command-errors.txt 
-sed -i 's/XKBLAYOUT="fr"/XKBLAYOUT="be"/' /etc/default/keyboard 1>> late_command-out.txt 2>> late_command-errors.txt 
+sed -i 's/XKBVARIANT="latin9"/XKBVARIANT="oss_sundeadkeys"/' /etc/default/keyboard
+sed -i 's/XKBLAYOUT="fr"/XKBLAYOUT="be"/' /etc/default/keyboard
 
-wget http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/oxygen/1a/eclipse-jee-oxygen-1a-linux-gtk-x86_64.tar.gz 1>> late_command-out.txt 2>> late_command-errors.txt 
+wget http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/oxygen/1a/eclipse-jee-oxygen-1a-linux-gtk-x86_64.tar.gz
 tar -x -f eclipse-jee-oxygen-1a-linux-gtk-x86_64.tar.gz -C /usr/local/share/
 ln -s /usr/local/share/eclipse/eclipse /usr/local/bin
-eclipse -nosplash -application org.eclipse.equinox.p2.director -repository "http://download.eclipse.org/releases/oxygen/,http://download.oracle.com/otn_software/oepe/12.2.1.6/oxygen/repository" -installIU oracle.eclipse.tools.glassfish.feature.group 1>> late_command-out.txt 2>> late_command-errors.txt 
+/usr/local/share/eclipse/eclipse -nosplash -application org.eclipse.equinox.p2.director -repository "http://download.eclipse.org/releases/oxygen/,http://download.oracle.com/otn_software/oepe/12.2.1.6/oxygen/repository" -installIU oracle.eclipse.tools.glassfish.feature.group
 xdg-icon-resource install --size 256 /usr/local/share/eclipse/icon.xpm eclipse-oxygen
 xdg-desktop-menu install "/media/cdrom/eclipse-oxygen.desktop"
 
